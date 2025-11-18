432b1b360d6f323adb650efd2542ad8a41fd5252
Celine Santos Pereira <celinesantospereira7@gmail.com>
2025-11-17 22:12:24 -0300

    Add files via upload

---
+++ ex2b2/ex2b2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nomes = new string[10];
+            int i;
+
+            for (i = 0; i < 10; i++)
+            {
+                Console.Write("Digite o {0}º nome: ", i+1);
+                nomes[i] = Console.ReadLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
